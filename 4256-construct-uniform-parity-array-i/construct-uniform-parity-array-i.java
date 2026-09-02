b70358e32622af35c6d1d0e4b83bef4204b8fe25
Trisanjeet268 <trisanjeetbhunia@gmail.com>
2026-09-02 18:40:07 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (16.73%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,30 +1,5 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-         boolean hasodd=false;
-         boolean haseven=false;
-
-         for(int i=0;i< nums1.length;i++){
-             if(nums1[i]%2==0){
-                 haseven=true;
-             }else {
-                 hasodd=true;
-             }
-         }
-         if(hasodd==false){
-             return true;
-         }
-         if(haseven == false){
-             return true;
-         }
-         for(int i=0;i< nums1.length;i++){
-             if(nums1[i]%2==0){
-                 for(int j=0;j< nums1.length;j++){
-                     if(nums1[j]%2!=0){
-                         nums1[i]=nums1[i]-nums1[j];
-                     }
-                 }
-             }
-         }
          return true;
     }
 }
